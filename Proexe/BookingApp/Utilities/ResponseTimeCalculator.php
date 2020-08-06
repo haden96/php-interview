032749bd83ed4a5b33aa07249b1312653d4c70af
haden96 <charkiewiczdaniel@gmail.com>
2020-08-06 19:55:05 +0200

    Task 4

--- Proexe/BookingApp/Utilities/ResponseTimeCalculator.php
+++ Proexe/BookingApp/Utilities/ResponseTimeCalculator.php
@@ -7,9 +7,91 @@
 
 namespace Proexe\BookingApp\Utilities;
 
+use DateTime;
+use Proexe\BookingApp\Offices\Interfaces\ResponseTimeCalculatorInterface;
 
-class ResponseTimeCalculator {
+class ResponseTimeCalculator implements ResponseTimeCalculatorInterface {
 
-	//Write your methods here
+    public function calculate($bookingDateTime, $responseDateTime, $officeHours)
+    {
+        $this->setVariables($bookingDateTime, $responseDateTime, $officeHours);
+
+        $time=[];
+        foreach ($this->officeHours as $key=>$value){
+            if($key == $this->bookingDayOfTheWeek && $key == $this->responseDayOfTheWeek){
+                array_push($time,[$key,$this->sameDayBookedAndResponse()]);
+                break;
+            }
+            if ($key == $this->bookingDayOfTheWeek){
+                array_push($time,[$key,$this->timeToEndOfTheDay()]);
+                --$this->noDays;
+                continue;
+            }else if($key == $this->responseDayOfTheWeek){
+                array_push($time,[$key,$this->timeFromStartOfTheDay()]);
+                --$this->noDays;
+                continue;
+            }else if ($this->noDays<0){
+                break;
+            }else{
+                array_push($time,[$key,$this->timeOpenedAllDay($key)]);
+                --$this->noDays;
+            }
+        }
+
+        return $time;
+    }
+
+    private function setVariables($bookingDateTime, $responseDateTime, $officeHours){
+        $this->bookingDateTime=new DateTime($bookingDateTime);
+        $this->responseDateTime=new DateTime($responseDateTime);
+        $this->officeHours=$officeHours;
+        $this->bookingDayOfTheWeek=$this->bookingDateTime->format('l');
+        $this->responseDayOfTheWeek=$this->responseDateTime->format('l');
+        $this->noDays=$this->numberOfDays();
+    }
+    private function numberOfDays(){
+        $start=new DateTime($this->bookingDateTime->format('Y-m-d'));
+        $end=new DateTime($this->responseDateTime->format('Y-m-d'));
+        return $start->diff($end)->format('%R%a');
+    }
+
+    private function timeOpenedAllDay($day){
+        $openTime=$this->officeHours[$day];
+        return $this->countMinutesBetweenTwoHours($openTime[0],$openTime[1]);
+    }
+
+    private function sameDayBookedAndResponse(){
+        $responseTime=$this->responseDateTime->format('H:i');
+        $bookingTime=$this->bookingDateTime->format('H:i');
+        return $this->countMinutesBetweenTwoHours($responseTime,$bookingTime);
+
+    }
+    private function timeToEndOfTheDay(){
+        $closeTime=$this->officeHours[$this->bookingDayOfTheWeek][1];
+        $bookingTime=$this->bookingDateTime->format('H:i');
+        return $this->countMinutesBetweenTwoHours($closeTime,$bookingTime);
+    }
+
+    private function timeFromStartOfTheDay(){
+        $openTime=$this->officeHours[$this->responseDayOfTheWeek][0];
+        $responseTime=$this->responseDateTime->format('H:i');
+        return $this->countMinutesBetweenTwoHours($responseTime,$openTime);
+    }
+
+    private function countMinutesBetweenTwoHours($hourLatter,$hourBefore){
+        $minutesEnd=$this->hoursToMinutes($hourLatter);
+        $minutesStart=$this->hoursToMinutes($hourBefore);
+        $result=$minutesEnd - $minutesStart;
+        return ($result>0)?$result:0;
+    }
+
+    private function hoursToMinutes($time){
+        $numbers=explode(':',$time);
+        if (count($numbers)<2){
+            return 0;
+        }else{
+            return (int)$numbers[0]*60+(int)$numbers[1];
+        }
+    }
 
 }
--- app/Console/Commands/CalculateResponseTime.php
+++ app/Console/Commands/CalculateResponseTime.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use DateTime;
 use Illuminate\Console\Command;
 use Proexe\BookingApp\Bookings\Models\BookingModel;
 use Proexe\BookingApp\Utilities\ResponseTimeCalculator;
@@ -39,9 +40,92 @@ class CalculateResponseTime extends Command
      */
     public function handle() {
         $bookings = BookingModel::with('office')->get()->toArray();
-        var_dump($bookings);
+        $responseTimeCalculator = new ResponseTimeCalculator();
 
-	    //Use ResponseTimeCalculator class for all calculations
-	    //You can use $this->line() to write out any info to console
+        $format = "Y-m-d H:i:s";
+        foreach ($bookings  as $booking ) {
+
+            $createAt = DateTime::createFromFormat($format, $booking['created_at'])->format('D');
+            $updatedAt = DateTime::createFromFormat($format, $booking['updated_at'])->format('D');
+
+            $this->line( "created_at = {$booking['created_at']} ({$createAt})");
+            $this->line( "updated_at = {$booking['updated_at']} ({$updatedAt})");
+            $this->line( "Opening hours:");
+            $hours = $this->hours($booking['office']['office_hours']);
+            foreach ($hours as $key=>$item){
+                if ($item[0]&&$item[1]=='0:00'){
+                    $this->line("{$key} : closed ");
+                }else{
+                $this->line("{$key} : {$item[0]} - {$item[1]} ");
+                }
+            };
+            $answer=$this->buildAnswer($responseTimeCalculator->calculate($booking['created_at'], $booking['updated_at'], $hours));
+            $this->line($answer);
+            $this->line(' ');
+
+        };
+    }
+    private function buildAnswer($items){
+        $response="Response time will be:";
+        $sum=0;
+        $length = count($items);
+        foreach ($items as $item){
+            if ($item[1]>0){
+                $response.=" ".$item[1]." min on ".$item[0];
+                $sum += $item[1];
+            }else{
+                continue;
+            };
+        };
+        $response.=" = ".$sum." minutes";
+        return $response;
+    }
+    private function hours($data)
+        {
+            $hours = [];
+            foreach ($data as $key => $item) {
+                if($item['isClosed'] === true) {
+                    $hours[$this->getWeekName($key)] = [
+                        '0:00',
+                        '0:00'
+                    ];
+                } else {
+                    $hours[$this->getWeekName($key)] = [
+                        $item['from'],
+                        $item['to']
+                    ];
+                }
+            }
+            return $hours;
+
+        }
+    private function getWeekName($key)
+    {
+        switch($key) {
+            case 0:
+                return 'Sunday';
+                break;
+            case 1:
+                return 'Monday';
+                break;
+            case 2:
+                return 'Tuesday';
+                break;
+            case 3:
+                return 'Wednesday';
+                break;
+            case 4:
+                return 'Thursday';
+                break;
+            case 5:
+                return 'Friday';
+                break;
+            case 6:
+                return 'Saturday';
+                break;
+            default:
+                throw new \Exception('Name of week not found');
+        }
+
     }
 }
