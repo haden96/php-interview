0f08b2c42698ad57766911a4522f13efb3cba1ab
haden96 <charkiewiczdaniel@gmail.com>
2020-08-06 17:38:08 +0200

    Task 3

--- Proexe/BookingApp/Utilities/DistanceCalculator.php
+++ Proexe/BookingApp/Utilities/DistanceCalculator.php
@@ -18,8 +18,20 @@ class DistanceCalculator {
 	 * @return mixed
 	 */
 	public function calculate( $from, $to, $unit = 'm' ) {
-		var_dump($from, $to, $unit);
-		//return $distance;
+        [$lat1,$lon1] = $from;
+        [$lat2,$lon2] = $to;
+
+        $theta = $lon1 - $lon2;
+        $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+        $dist = acos($dist);
+        $dist = rad2deg($dist);
+        $miles = $dist * 60 * 1.1515;
+        $km=$miles * 1.609344;
+        if ($unit == "km") {
+            return $km;
+        } else if ($unit == "m") {
+            return ($km*1000);
+        }
 	}
 
 	/**
@@ -30,7 +42,25 @@ class DistanceCalculator {
 	 */
 	public function findClosestOffice( $from, $offices ) {
 
-		//return $office;
-	}
+        $results = [];
+		foreach ($offices as $office){
+            array_push($results,$this->calculate($from,[$office['lat'],$office['lng']], $unit = 'm'));
+        }
+        $position=array_keys($results, min($results));
+        return $offices[$position[0]]['name'];
+    }
 
+// CREATE
+// FUNCTION `calculate`(LatA DECIMAL(20,8), LngA DECIMAL(20,8), LatB DECIMAL(20,8), LngB DECIMAL(20,8))
+// RETURNS DECIMAL(20,8)
+// RETURN 60 * 1.1515 * 1.609344*
+// DEGREES(ACOS(LEAST(1.0,
+// SIN(RADIANS(LatA))
+//  * SIN(RADIANS(LatB))
+//  + COS(RADIANS(LatA))
+//  * COS(RADIANS(LatB))
+//  * COS(RADIANS(LngA - LngB))
+// )));
+
+// SELECT o.`id`, calculate($lat, $lng, o.`lat`, o.`lng`) as `km` FROM `offices` o ORDER BY `km` ASC LIMIT 1
 }
